71f74a00b261f1a682f3c51bd3f8ff3dfe3195fd
Amazingmercy <amaefulemercy499@gmail.com>
2025-03-16 15:32:12 +0100

    Modified admin settings

--- routers/adminProductRoutes.js
+++ routers/adminProductRoutes.js
@@ -11,9 +11,9 @@ router.use(adminMiddleware)
 // Admin products routes
 router.get('/products', getProducts);
 router.get('/products/add', getAddProduct);
-router.post('/products/add', upload.array('productImage', 3), postAddProduct)
+router.post('/products/add', upload.array('productImage', 5), postAddProduct)
 router.get('/products/edit/:id', getEditProduct);
-router.post('/products/edit/:id', upload.array('productImage', 3), postEditProduct);
+router.post('/products/edit/:id', upload.array('productImage', 5), postEditProduct);
 router.post('/products/outOfStock/:id', outOfStockProduct);
 router.post('/products/trending/:id', addToTrend);
 router.post('/deliveryPrice/update', updateOrCreateDeliveryPrices);
